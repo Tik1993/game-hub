afffbffc4c07014ff5d123b3c3aa07ca585f222d
Ernest Lin <linchongtik@gmail.com>
2023-09-23 13:36:13 -0700

    install Chakra UI and create a responsive layout

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav"
+                  "aside main"`,
+        }}
+      >
+        <GridItem bg="orange.300" area={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="pink.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
