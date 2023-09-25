07c251705eeb45592769a44b6cab4d5e0e634d12
Ernest Lin <linchongtik@gmail.com>
2023-09-24 19:52:53 -0700

    Displaying platform icons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
     params:{
-           key:'d01488b34d224f8da5712a91b4396379'
+             key:'d01488b34d224f8da5712a91b4396379'
     }
 })
