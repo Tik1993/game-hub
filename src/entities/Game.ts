00fef8319700b291dd0fb595b52661682d47be04
Ernest Lin <linchongtik@gmail.com>
2023-10-07 14:54:40 -0700

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,5 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
-
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
@@ -9,4 +10,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform; }[];
   metacritic: number;
+  genres:Genre[];
+  publishers:Publisher[];
+
 }
